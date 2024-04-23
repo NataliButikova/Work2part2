ee85bcf9573a0355f989fcbc20fb79f5c9db63e3
Natali Butikova <Butikova87@bk.ru>
2024-04-23 14:10:16 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100; // денежные средства на счете
+        int payment = 1100; // сумма пополнения
+
+        int percent;
+
+        if (payment > 1000) {percent = 1;}
+        else {
+            percent = 0;
+
+        }
+
+        int bonus = payment * percent / 100;
+
+        System.out.println( "Итоговая сумма на счете " + (account + payment) );
+        System.out.println( "Итоговая сумма бонусов " + bonus);
+
+
+    }
+}
